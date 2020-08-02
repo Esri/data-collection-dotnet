9d020935a5844abcc58678560695fd0fef63b98c
Nathan Castle <ncastle@esri.com>
2020-08-01 22:45:12 -0700

    Panel appearance, layout improvements, tooltips

---
+++ src/DataCollection.UWP/Converters/DateTimeStringFormatConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Esri.ArcGISRuntime.OpenSourceApps.DataCollection.UWP.Converters
+{
+    public class DateTimeStringFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (parameter is string format)
+            {
+                if (value is DateTimeOffset dto)
+                {
+                    return dto.ToString(format);
+                }
+                else if (value is DateTime date)
+                {
+                    return date.ToString(format);
+                }
+            }
+
+            return value?.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
